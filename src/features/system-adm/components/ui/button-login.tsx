c688753813cac04d95f8918e9593f244681a597d
Luis Dallas <luiseduard2005@hotmail.com>
2025-04-26 16:04:12 -0300

    style: estilizacao inicial

--- src/features/system-adm/components/ui/button-login.tsx
+++ src/features/system-adm/components/ui/button-login.tsx
@@ -6,7 +6,7 @@ interface ButtonLoginProps {
 
 export function ButtonLogin({ children, onClick, type = "button" }: ButtonLoginProps) {
     return (
-        <button onClick={onClick} type={type}>
+        <button onClick={onClick} type={type} className="bg-[#FE3E00] text-white w-[330px] h-[54px] rounded-[10px] font-medium font-sans text-lg">
             {children}
         </button>
     );
