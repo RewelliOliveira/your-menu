33aca56de6e7d56f241d4d191cb50d437d23b2c3
Julian Cardoso <juliancesarcardoso@gmail.com>
2025-04-26 15:04:52 -0300

    ::sparkles::feat:className e pagina de login

--- src/features/system-adm/components/ui/input-login.tsx
+++ src/features/system-adm/components/ui/input-login.tsx
@@ -1,19 +1,29 @@
-interface InputLoginProps {
-    label: string,
-    type?: string,
-    value?: string,
-    onChange?: (e: React.ChangeEvent<HTMLInputElement>) => void;
+import "./input-login.css";
 
+interface InputLoginProps {
+  placeholder: string;
+  label: string;
+  type?: string;
+  value?: string;
+  onChange?: (e: React.ChangeEvent<HTMLInputElement>) => void;
 }
-export function InputLogin({ label, type = "text", value, onChange }: InputLoginProps) {
-    return (
-        <div>
-            <label htmlFor="">{label}</label>
-            <input
-                type={type}
-                value={value}
-                onChange={onChange}
-            />
-        </div>
-    );
+
+export function InputLogin({
+  placeholder,
+  label,
+  type = "text",
+  value,
+  onChange,
+}: InputLoginProps) {
+  return (
+    <div className="input-container">
+      <label htmlFor="">{label}</label>
+      <input
+        placeholder={placeholder}
+        type={type}
+        value={value}
+        onChange={onChange}
+      />
+    </div>
+  );
 }
--- src/features/system-adm/pages/login-adm.tsx
+++ src/features/system-adm/pages/login-adm.tsx
@@ -1,5 +1,13 @@
-export function Login (){
-    return(
-        <div>Login aqui</div>
-    );
+import { InputLogin } from "../components/ui/input-login";
+
+export function LoginAdm() {
+  return (
+    <div>
+      <h1>Login</h1>
+      <form>
+        <InputLogin placeholder="Digite seu email" label="Email" />
+        <InputLogin placeholder="Digite sua senha" label="Senha" />
+      </form>
+    </div>
+  );
 }
