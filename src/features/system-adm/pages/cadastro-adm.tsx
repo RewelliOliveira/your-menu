55f289060b9463d2e85e12b7fb0542f171adb591
Julian Cardoso <juliancesarcardoso@gmail.com>
2025-04-27 16:20:00 -0300

    Style:tela de cadastro atualizada

--- src/features/system-adm/pages/cadastro-adm.tsx
+++ src/features/system-adm/pages/cadastro-adm.tsx
@@ -3,28 +3,29 @@ import { InputLogin } from "../components/ui/input-login";
 
 export function Cadastro() {
   return (
-    <div className="flex flex-col text-center justify-center gap-10">
-      <header className="flex flex-col gap-5">
-        <div>logo aqui</div>
-        <h3>Your menu</h3>
+    <div className="min-h-screen flex flex-col text-center justify-start gap-5 mx-auto max-w-md px-4 pt-40 sm:pt-10 md:pt-40 lg:pt-70">
+      <header className="flex flex-col gap-5 ">
+        <div className="justify-center w-70 sm:w-50 md:w-100 mx-auto">
+          <img src="/logo.svg" alt="Logo YourMenu" />
+        </div>
       </header>
-      <main>
-        <div>
-          <InputLogin label="Email" placeholder="Digite seu email" />
-        </div>
-        <div>
+
+      <div className="mt-8 bg-white py-6 sm:py-6 rounded-lg px-4 sm:px-4 md:px-8 lg:px-12">
+        <main>
+          <InputLogin label="Email" placeholder="email@dominio.com" />
           <InputLogin label="Senha" placeholder="Digite uma senha forte" />
-        </div>
-        <div>
           <InputLogin label="Repita sua senha" placeholder="Digite novamente" />
-        </div>
-        <p>Esqueceu sua senha?</p>
-      </main>
-      <footer>
+        </main>
+      </div>
+
+      <footer className="flex flex-col items-center gap-4 mt-6">
         <ButtonLogin type="submit">Cadastrar</ButtonLogin>
-        <p>
-          ja possui uma conta? <span>Entre agora</span>
-        </p>
+        <div className="text-lg text-black">
+          <p>
+            Ja tem uma conta?
+            <span className="text-orange-600 font-bold ml-1">Entre agora</span>
+          </p>
+        </div>
       </footer>
     </div>
   );
