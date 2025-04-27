ce4eb3358b4e491ef78a15a21291323d55492ebb
Julian Cardoso <juliancesarcardoso@gmail.com>
2025-04-27 16:13:27 -0300

    Refact: ajuste de erro

--- src/features/system-adm/components/ui/button-login.tsx
+++ src/features/system-adm/components/ui/button-login.tsx
@@ -13,7 +13,7 @@ export function ButtonLogin({
     <button
       onClick={onClick}
       type={type}
-      className="bg-[#FE3E00] text-white w-full sm:w-[330px] h-14 rounded-[10px] font-medium font-sans text-lg"
+      className="bg-orange-600 text-white w-4/5 sm:w-1/2 h-14 rounded-[10px] font-medium font-sans text-lg"
     >
       {children}
     </button>
--- src/features/system-adm/pages/login-adm.tsx
+++ src/features/system-adm/pages/login-adm.tsx
@@ -4,18 +4,18 @@ import { InputLogin } from "../components/ui/input-login";
 
 export function LoginAdm() {
   return (
-    <div className="min-h-screen flex flex-col text-center justify-start gap-5 mx-auto max-w-md px-4 pt-30 sm:pt-30 md:pt-40 lg:pt-50">
+    <div className="min-h-screen flex flex-col text-center justify-start gap-5 mx-auto max-w-md px-4 pt-40 sm:pt-10 md:pt-40 lg:pt-70">
       <header className="flex flex-col gap-5 ">
-        <div className="justify-center w-50 sm:w-50 md:w-60 mx-auto">
+        <div className="justify-center w-70 sm:w-50 md:w-100 mx-auto">
           <img src="/logo.svg" alt="Logo YourMenu" />
         </div>
       </header>
 
-      <div className="mt-8 bg-white py-6 sm:py-8 rounded-lg px-4 sm:px-6 md:px-8 lg:px-10">
+      <div className="mt-8 bg-white py-6 sm:py-6 rounded-lg px-4 sm:px-4 md:px-8 lg:px-12">
         <main>
           <InputLogin label="Email" placeholder="email@dominio.com" />
           <InputLogin label="Senha" placeholder="********" />
-          <p className="text-sm text-right text-orange-600">
+          <p className=" font-bold text-base text-right text-orange-600">
             Esqueceu sua senha?
           </p>
         </main>
@@ -23,9 +23,9 @@ export function LoginAdm() {
 
       <footer className="flex flex-col items-center gap-4 mt-6">
         <ButtonLogin type="submit">Entrar</ButtonLogin>
-        <div className="text-base text-black">
+        <div className="text-lg text-black">
           <p>
-            Não possui uma conta?
+            Não tem uma conta?
             <span className="text-orange-600 font-bold ml-1">Crie agora</span>
           </p>
         </div>
