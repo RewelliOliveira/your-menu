2e5dbeaad397e11009c528f5e499d21ae50d8c92
RewelliOliveira <rewelli@alu.ufc.br>
2025-04-29 20:35:14 -0300

    feat: rotas de login e cadastro

--- src/features/system-adm/pages/cadastro-adm.tsx
+++ src/features/system-adm/pages/cadastro-adm.tsx
@@ -1,9 +1,10 @@
+import { Link } from "react-router-dom";
 import { ButtonLogin } from "../components/ui/button-login";
 import { InputLogin } from "../components/ui/input-login";
 
 export function Cadastro() {
   return (
-    <div className="min-h-screen flex flex-col text-center justify-start gap-5 mx-auto max-w-md px-4 pt-40 sm:pt-10 md:pt-40 lg:pt-70">
+    <div className="min-h-screen flex flex-col text-center justify-start gap-5 mx-auto max-w-md px-4 mt-20">
       <header className="flex flex-col gap-5 ">
         <div className="justify-center w-70 sm:w-50 md:w-100 mx-auto">
           <img src="/logo.svg" alt="Logo YourMenu" />
@@ -23,7 +24,9 @@ export function Cadastro() {
         <div className="text-lg text-black">
           <p>
             Ja tem uma conta?
-            <span className="text-orange-600 font-bold ml-1">Entre agora</span>
+            <Link to="/">
+              <span className="text-orange-600 font-bold ml-1">Entre agora</span>
+            </Link>
           </p>
         </div>
       </footer>
