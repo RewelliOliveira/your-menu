4b6aefe75b53a38380f4e795f5c91ee4c9de20ac
Julian Cardoso <juliancesarcardoso@gmail.com>
2025-05-29 22:04:24 -0300

    feat: cards e responsividade

--- src/features/system-adm/components/order.tsx
+++ src/features/system-adm/components/order.tsx
@@ -1,44 +1,27 @@
 import { Icon } from "@iconify/react";
-import { useEffect, useState } from "react";
 
-export function Order() {
-  const [data, setData] = useState({
-    foodImg: "",
-    name: "",
-    description: "",
-    price: "",
-  });
+export type OrderProps = {
+  name: string;
+  description: string;
+  price: string;
+  foodImg: string;
+};
 
-  useEffect(() => {
-    const fetchData = async () => {
-      const response = {
-        foodImg:
-          "https://i.pinimg.com/736x/24/c6/a7/24c6a7e6e183d78d9d1beea77b6d160e.jpg",
-        name: "Lasanha",
-        description:
-          "Lasanha de carne com molho especial muito bom e gostoso meu deus do céu que legal ficou isso aqui.",
-        price: "29,90",
-      };
-      setData(response);
-    };
-
-    fetchData();
-  }, []);
-
+export function Order(order: OrderProps) {
   return (
     <div className="flex flex-col bg-white w-55 h-60 m-4 rounded-sm shadow-xl/10 transition-all duration-300 hover:shadow-xl hover:scale-[1.02]">
       <img
-        src={data.foodImg || "placeholder.svg"}
-        alt="Imagem da comida"
+        src={order.foodImg || "placeholder.svg"}
+        alt=""
         className="w-full h-24 object-cover rounded-t"
       />
       <div className="p-3 flex flex-col flex-1">
-        <h2 className="font-bold text-base">{data.name}</h2>
+        <h2 className="font-bold text-base">{order.name || "Nome do prato"}</h2>
         <p className="text-xs text-gray-500 break-words overflow-hidden">
-          {data.description}
+          {order.description || "Descrição do prato"}
         </p>
         <div className="flex justify-between items-end flex-1 mt-2">
-          <p className="font-semibold">R${data.price}</p>
+          <p className="font-semibold">R${order.price || " --,--"}</p>
           <div className="bg-white shadow-md w-8 h-8 flex items-center justify-center rounded">
             <Icon icon="solar:pen-2-outline" className="w-5 h-5" />
           </div>
