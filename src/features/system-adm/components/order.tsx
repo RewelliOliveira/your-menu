85a36f328003910789a317d2ff9c5c7ec496d437
Julian Cardoso <juliancesarcardoso@gmail.com>
2025-05-29 22:16:50 -0300

    style: cor do icone

--- src/features/system-adm/components/order.tsx
+++ src/features/system-adm/components/order.tsx
@@ -9,11 +9,11 @@ export type OrderProps = {
 
 export function Order(order: OrderProps) {
   return (
-    <div className="flex flex-col bg-white w-55 h-60 m-4 rounded-sm shadow-xl/10 transition-all duration-300 hover:shadow-xl hover:scale-[1.02]">
+    <div className="flex flex-col bg-white w-55 h-60 m-4 rounded-xl shadow-xl/15 transition-all duration-300 hover:shadow-xl hover:scale-[1.02]">
       <img
         src={order.foodImg || "placeholder.svg"}
         alt=""
-        className="w-full h-24 object-cover rounded-t"
+        className="w-full h-24 object-cover rounded-t-lg"
       />
       <div className="p-3 flex flex-col flex-1">
         <h2 className="font-bold text-base">{order.name || "Nome do prato"}</h2>
@@ -22,9 +22,17 @@ export function Order(order: OrderProps) {
         </p>
         <div className="flex justify-between items-end flex-1 mt-2">
           <p className="font-semibold">R${order.price || " --,--"}</p>
-          <div className="bg-white shadow-md w-8 h-8 flex items-center justify-center rounded">
-            <Icon icon="solar:pen-2-outline" className="w-5 h-5" />
-          </div>
+          <button
+            onClick={() => console.log("Abrir modal")}
+            type="button"
+            aria-label="Editar prato"
+            className="group bg-white shadow-xl/15 w-8 h-8 flex items-center justify-center transition-all duration-300 rounded"
+          >
+            <Icon
+              icon="solar:pen-2-outline"
+              className="w-5 h-5 text-gray-500 transition-colors duration-300 group-hover:text-orange-600"
+            />
+          </button>
         </div>
       </div>
     </div>
