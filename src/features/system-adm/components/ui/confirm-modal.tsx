c213de15504d01f3b93ed593a343d22adacaca57
Julian Cardoso <juliancesarcardoso@gmail.com>
2025-07-02 17:43:58 -0300

    style: mudança para cor amigavel

--- src/features/system-adm/components/ui/confirm-modal.tsx
+++ src/features/system-adm/components/ui/confirm-modal.tsx
@@ -1,48 +1,46 @@
-import React from 'react';
-import { X } from 'lucide-react';
-import { Button } from './button'; // Importe seu componente Button
+import { X } from "lucide-react";
+import React from "react";
+import { Button } from "./button"; // Importe seu componente Button
 
 interface ConfirmModalProps {
-    message: string;
-    buttonmsg: string;
-    onConfirm: () => void;
-    onCancel: () => void;
-    isOpen: boolean;
+  message: string;
+  buttonmsg: string;
+  onConfirm: () => void;
+  onCancel: () => void;
+  isOpen: boolean;
 }
 
 export const ConfirmModal: React.FC<ConfirmModalProps> = ({
-    message,
-    buttonmsg,
-    onConfirm,
-    onCancel,
-    isOpen,
+  message,
+  buttonmsg,
+  onConfirm,
+  onCancel,
+  isOpen,
 }) => {
-    if (!isOpen) return null;
-  
-    return (
-        <div className="fixed inset-0 flex items-center justify-center bg-black/20 z-50">
-            <div className="bg-white p-8 rounded shadow-lg text-black w-[420px] relative">
-                <button
-                    onClick={onCancel}
-                    className="absolute top-2 right-2 text-gray-500 hover:text-black"
-                >
-                    <X className="w-5 h-5" />
-                </button>
+  if (!isOpen) return null;
 
-                <p className="mb-6 text-lg font-semibold">
-                    {message}
-                </p>
+  return (
+    <div className="fixed inset-0 flex items-center justify-center bg-black/20 z-50">
+      <div className="bg-white p-8 rounded shadow-lg text-black w-[420px] relative">
+        <button
+          onClick={onCancel}
+          className="absolute top-2 right-2 text-gray-500 hover:text-black"
+        >
+          <X className="w-5 h-5" />
+        </button>
 
-                <div className="flex justify-end">
-                    <Button
-                        onClick={onConfirm}
-                        type="button"
-                        className="!bg-red-600 hover:!bg-red-700 !px-3 !py-1.5 !h-auto !w-auto !text-base"
-                    >
-                        {buttonmsg}
-                    </Button>
-                </div>
-            </div>
+        <p className="mb-6 text-lg font-semibold">{message}</p>
+
+        <div className="flex justify-end">
+          <Button
+            onClick={onConfirm}
+            type="button"
+            className="!bg-orange-600 hover:!bg-orange-700 !px-3 !py-1.5 !h-auto !w-auto !text-base"
+          >
+            {buttonmsg}
+          </Button>
         </div>
-    );
+      </div>
+    </div>
+  );
 };
