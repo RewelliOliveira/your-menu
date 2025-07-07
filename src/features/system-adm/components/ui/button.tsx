092048c5291a58a13ab34616db3e7a7fbca6323f
Luis Dallas <luiseduard2005@hotmail.com>
2025-07-07 08:35:24 -0300

    Resolvendo conflito

--- src/features/system-adm/components/ui/button.tsx
+++ src/features/system-adm/components/ui/button.tsx
@@ -3,7 +3,6 @@ interface ButtonProps {
   onClick?: () => void;
   type?: "button" | "submit" | "reset";
   className?: string;
-  disabled?: boolean;
 }
 
 export function Button({
@@ -11,16 +10,14 @@ export function Button({
   onClick,
   type = "button",
   className = "",
-  disabled = false,
 }: ButtonProps) {
   const baseClasses =
-    "bg-orange-600 text-[#FFFFFF] w-4/5 sm:w-1/2 h-14 rounded-[10px] font-medium font-sans text-lg lg:h-11 lg:w-3/5 hover:bg-orange-500 cursor-pointer disabled:opacity-50 disabled:cursor-not-allowed";
+    "bg-orange-600 text-[#FFFFFF] w-4/5 sm:w-1/2 h-14 rounded-[10px] font-medium font-sans text-lg lg:h-11 lg:w-3/5 hover:bg-orange-500 cursor-pointer";
 
   return (
     <button
       onClick={onClick}
       type={type}
-      disabled={disabled}
       className={`${baseClasses} ${className}`}
     >
       {children}
