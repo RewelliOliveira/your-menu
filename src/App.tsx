7ac3b104be9fe172d4e81ae2404f32eefe320d28
RewelliOliveira <rewelli@alu.ufc.br>
2025-07-16 14:24:45 -0300

    feat: criando rotas de client

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,14 @@
 import "./App.css";
+import { BrowserRouter } from "react-router-dom";
 import { Router } from "./router/router";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 
 export function App() {
   return (
-    <>
+    <BrowserRouter>
       <Router />
       <ToastContainer />
-    </>
+    </BrowserRouter>
   );
 }
