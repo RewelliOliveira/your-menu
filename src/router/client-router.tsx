06170bb3214d8b58b3587bdfa72be2abd4504cff
Julian Cardoso <juliancesarcardoso@gmail.com>
2025-07-21 18:40:38 -0300

    feat: adição das telas nas rotas

--- src/router/client-router.tsx
+++ src/router/client-router.tsx
@@ -1,10 +1,16 @@
-import { Routes, Route } from "react-router-dom";
+import { AddressData } from "@/features/system-client/pages/address-data";
+import { CheckOrder } from "@/features/system-client/pages/check-order";
 import { MenuClient } from "@/features/system-client/pages/menu-client";
+import { PersonalData } from "@/features/system-client/pages/personal-data";
+import { Route, Routes } from "react-router-dom";
 
 export default function ClientRoutes() {
   return (
     <Routes>
       <Route path="/" element={<MenuClient />} />
+      <Route path="/check-order" element={<CheckOrder />} />
+      <Route path="/personal-data" element={<PersonalData />} />
+      <Route path="/address-data" element={<AddressData />} />
     </Routes>
   );
 }
