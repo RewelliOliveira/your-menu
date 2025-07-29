f6af67d6c35317ba0b59f2395a943d09c4905337
Julian Cardoso <juliancesarcardoso@gmail.com>
2025-07-29 19:00:51 -0300

    fix: Ajuste dos erros

--- src/features/system-client/pages/payment.tsx
+++ src/features/system-client/pages/payment.tsx
@@ -3,40 +3,40 @@ import { Link } from "react-router-dom";
 
 export function PaymentMethod() {
   return (
-    <section className="flex flex-col items-center justify-center w-full min-h-screen p-4 bg-white">
+    <div className="flex flex-col bg-[#f5f5f5] items-center justify-center min-h-screen gap-4">
       <div className="flex flex-col w-full max-w-3xl p-6 bg-[#f5f5f5] rounded-lg border border-gray-400 shadow-md">
-        <h1 className="flex w-full justify-center text-2xl font-bold text-black mb-6">
+        <h1 className="flex w-full justify-center text-xl font-medium text-black/80 mb-6">
           Forma de pagamento
         </h1>
 
-        <div className="flex flex-col gap-3 p-4">
+        <div className="flex flex-col gap-3">
           <Link
             to="/payment/pix"
-            className="flex items-center gap-3 px-4 py-3 border rounded-md bg-white hover:bg-gray-100 transition"
+            className="flex items-center gap-3 px-4 py-3 border rounded-md bg-white hover:bg-gray-100"
           >
             <Icon
               icon="simple-icons:pix"
               width={24}
               className="text-green-500"
             />
-            <span className="text-base font-medium">Pix</span>
+            <span>Pix</span>
           </Link>
 
-          <button className="flex items-center gap-3 px-4 py-3 border rounded-md bg-white hover:bg-gray-100 transition">
+          <button className="flex items-center gap-3 px-4 py-3 border rounded-md bg-white hover:bg-gray-100">
             <Icon
               icon="mdi:credit-card-outline"
               width={24}
               className="text-black"
             />
-            <span className="text-base font-medium">Cartão</span>
+            <span>Cartão</span>
           </button>
 
-          <button className="flex items-center gap-3 px-4 py-3 border rounded-md bg-white hover:bg-gray-100 transition">
+          <button className="flex items-center gap-3 px-4 py-3 border rounded-md bg-white hover:bg-gray-100">
             <Icon icon="mdi:cash" width={24} className="text-yellow-600" />
-            <span className="text-base font-medium">Dinheiro</span>
+            <span>Dinheiro</span>
           </button>
         </div>
       </div>
-    </section>
+    </div>
   );
 }
--- src/features/system-client/pages/pix-payment.tsx
+++ src/features/system-client/pages/pix-payment.tsx
@@ -5,15 +5,14 @@ import { Button } from "../components/ui/button";
 export function PixPayment() {
   const qrCodeLink = "hbdajhbd333432-dsfsfsdfsdf-sdfsdfs-34342e";
   const navigate = useNavigate();
-
   return (
-    <section className="flex flex-col items-center justify-center w-full min-h-screen p-4 bg-white">
+    <div className="flex flex-col bg-[#f5f5f5] items-center justify-center min-h-screen">
       <div className="flex flex-col w-full max-w-3xl p-6 bg-[#f5f5f5] rounded-lg border border-gray-400 shadow-md">
-        <h1 className="self-center text-2xl font-bold text-black mb-6">
-          QR Code
+        <h1 className="self-center text-xl font-medium text-black/80 mb-4">
+          QrCode
         </h1>
 
-        <div className="self-center mb-6">
+        <div className="self-center mb-4">
           <img
             src="https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=example"
             alt="QR Code"
@@ -21,18 +20,18 @@ export function PixPayment() {
           />
         </div>
 
-        <div className="flex flex-wrap items-center justify-center gap-2 mb-6 px-2 text-center">
+        <div className="flex items-center gap-2 mb-6 flex-wrap justify-center">
           <Icon
             icon="mdi:content-copy"
             className="text-gray-500 cursor-pointer"
           />
-          <span className="text-sm text-red-500 break-words">{qrCodeLink}</span>
+          <span className="text-sm text-red-500 break-all">{qrCodeLink}</span>
         </div>
 
         <div className="flex flex-wrap gap-4 w-full justify-center">
           <Button
             variant="dark"
-            className="w-40 flex items-center justify-center gap-1"
+            className="!w-45 flex items-center justify-center gap-1"
             onClick={() => navigate("/")}
           >
             <Icon icon="mdi:keyboard-return" />
@@ -41,7 +40,7 @@ export function PixPayment() {
 
           <Button
             variant="primary"
-            className="bg-green-600 hover:bg-green-500 w-60 flex items-center justify-center gap-1"
+            className="!bg-green-600 hover:!bg-green-500 !w-60 flex items-center justify-center gap-1"
             onClick={() => console.log("Compartilhar")}
           >
             <Icon icon="mdi:whatsapp" />
@@ -49,6 +48,6 @@ export function PixPayment() {
           </Button>
         </div>
       </div>
-    </section>
+    </div>
   );
 }
